33ec2c2a86036aea143172d8188c9d1a0c2f1414
cutelee <soyoung.dev@gmail.com>
2019-12-01 14:10:51 +0900

    DB reconnection 코드 추가

--- backend_server/src/models/index.js
+++ backend_server/src/models/index.js
@@ -4,21 +4,42 @@ const config = require('../../config')[env];
 
 const db = {};
 
-const sequelize = new Sequelize(
-  config.DB.name, // 데이터베이스 이름
-  config.DB.user, // 유저 명
-  config.DB.password, // 비밀번호
-  {
-    'host': 'localhost', // 데이터베이스 호스트
-    'dialect': 'mysql', // 사용할 데이터베이스 종류
-    'port': 3307,
-  }
-);
+try {
+  const sequelize = new Sequelize(
+    config.DB.name, // 데이터베이스 이름
+    config.DB.user, // 유저 명
+    config.DB.password, // 비밀번호
+    {
+      'host': 'localhost', // 데이터베이스 호스트
+      'dialect': 'mysql', // 사용할 데이터베이스 종류
+      'port': 3306,
+      retry: {
+        match: [
+          /SequelizeConnectionError/,
+          /SequelizeConnectionRefusedError/,
+          /SequelizeHostNotFoundError/,
+          /SequelizeHostNotReachableError/,
+          /SequelizeInvalidConnectionError/,
+          /SequelizeConnectionTimedOutError/,
+        ],
+        name: 'query',
+        backoffBase: 100,
+        backoffExponent: 1.1,
+        timeout: 60000,
+        max: Infinity
+      }
+    }
+  );
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
 
-db.User = require('./user')(sequelize, Sequelize);
-db.User.sync();
+  db.User = require('./user')(sequelize, Sequelize);
+  db.User.sync();
+} catch (err) {
+  console.log('데이터베이스 연결 실패');
+}
+
+
 
 module.exports = db;
